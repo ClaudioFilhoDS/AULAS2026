74975d72cd060055ba91cbee9984f871bf67287f
claudiodelicia <cacgf1@aluno.ifal.edu.br>
2026-05-15 17:38:36 -0300

    tabuada + condicional com break

---
+++ ESTUDOS/2026/LDSOER.TEEPI/python/condicional+break.py
@@ -0,0 +1,37 @@
+x = 0
+
+while x < 5:
+
+    if x == 3:
+        break
+    print(x)
+    x += 1
+
+
+print("\n")
+#forma certa
+print("\n")
+
+
+x = 0
+
+while x < 3:
+    print(x)
+    
+    x += 1
+
+
+print("\n")
+#forma com o continue
+print("\n")
+
+x = 0
+
+while x < 5:
+
+    x += 1
+    if x == 3:
+        continue
+    print(x)
+    
+
---
+++ ESTUDOS/2026/LDSOER.TEEPI/python/tabuada.py
@@ -0,0 +1,10 @@
+x = 1
+
+while x < 11:
+    print(f"\n------ TABUADA DO {x} ------\n")
+    y = 1
+    while y < 11:
+        print(f"{x} x {y}= {x * y}")
+        y += 1
+    x +=1
+    
