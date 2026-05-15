0a4534fd8c36f403787dc176b5bb1ca6949f45c5
claudiodelicia <cacgf1@aluno.ifal.edu.br>
2026-05-15 16:52:03 -0300

    Duas atividades de condicional while

---
+++ ESTUDOS/2026/LDSOER.TEEPI/python/attcondicional.py
@@ -0,0 +1,11 @@
+linha1 = 0
+
+while linha1 < 5:
+    linha2 = 0
+    if linha1 > 0:
+        linha2 = linha1 
+    while linha2 < 5:
+        print(linha1, linha2)
+        linha2 += 1
+        
+    linha1 += 1
---
+++ ESTUDOS/2026/LDSOER.TEEPI/python/attestrutura.py
@@ -0,0 +1,9 @@
+linha1 = 0
+
+while linha1 < 3:
+    linha2 = 0
+    while linha2 < 4:
+        print(linha1, linha2)
+    
+        linha2 += 1
+    linha1 += 1
