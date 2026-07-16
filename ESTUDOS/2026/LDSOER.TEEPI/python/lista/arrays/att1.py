8ffd0b24f3e6eeb19404013b2463e7e15a1f7dfa
claudiodelicia <cacgf1@aluno.ifal.edu.br>
2026-07-16 14:14:00 -0300

    listas insert e append1

---
+++ ESTUDOS/2026/LDSOER.TEEPI/python/lista/arrays/att1.py
@@ -0,0 +1,16 @@
+lista = []
+
+print(lista)
+lista.append(1)
+print(lista)
+lista.append('1')
+print(lista)
+
+#o append insere sempre atras
+
+#o insert coloca elementos na lista, determinado pelo local
+
+lista.insert(0, 10)
+print(lista)
+
+
---
+++ ESTUDOS/2026/LDSOER.TEEPI/python/lista/arrays/att2.py
@@ -0,0 +1,8 @@
+lista = ['um', 2, 4]
+lista.insert(1, 1)
+lista.insert(3, 'tres')
+lista.insert(4, 'quatro')
+lista.insert(4, 3)
+lista.insert(2, 'dois')
+print(lista)
+
