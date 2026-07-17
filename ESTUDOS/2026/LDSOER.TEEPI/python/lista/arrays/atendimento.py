9bee24a84a543e20a59a9bf6ebd94555a8fe45c0
claudiodelicia <cacgf1@aluno.ifal.edu.br>
2026-07-17 15:59:16 -0300

    atendimento.py

---
+++ ESTUDOS/2026/LDSOER.TEEPI/python/lista/arrays/atendimento.py
@@ -0,0 +1,10 @@
+lista = []
+lista.append('ana')
+lista.append('claudia')
+
+print(f'primeiro atendimento {lista[0]}')
+lista.remove('ana')
+print(f"posições {lista}")
+lista.append('isabel')
+lista.append('paula')
+
